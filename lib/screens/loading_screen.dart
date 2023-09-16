6dce56645058aec60fc7622eedb87a166519cf08
selinguz <selin-guzel@hotmail.com>
2023-09-17 00:12:24 +0300

    json data dan veri çekme uygulaması yapıldı

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -22,12 +23,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    http.Response response = await http.get(Uri.parse('https://samples'
-        '.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
+    http.Response response = await http.get(Uri.parse('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
 
     if (response.statusCode == 200) {
       String data = response.body;
-      print(data);
+
+      var decodedData = jsonDecode(data);
+
+      double temperature = decodedData['main']['temp'];
+      print(temperature);
+      int conditionNumber = decodedData['weather'][0]['id'];
+      print(conditionNumber);
+      String cityName = decodedData['name'];
+      print(cityName);
+
     } else {
       print(response.statusCode);
     }
