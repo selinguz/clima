322db6ac956d40143ffd4d0bbccd9f634a43b229
selinguz <selin-guzel@hotmail.com>
2023-09-16 23:03:37 +0300

    http package ı kullanılarak getData metodu oluşturuldu, json formatında data gelmesi sağlandı

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,19 +9,28 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
-
-  void initState(){
+  void initState() {
     super.initState();
     getLocation();
   }
-
 
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
     print(location.longitude);
     print(location.latitude);
+  }
+
+  void getData() async {
+    http.Response response = await http.get(Uri.parse('https://samples'
+        '.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   Future<Position> _determinePosition() async {
@@ -41,6 +51,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Center(
         child: ElevatedButton(
